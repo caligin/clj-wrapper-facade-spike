784749cdc7691589e495cc59f2b6d4b74907ade7
Fulvio Meden <fmeden@thoughtworks.com>
2016-10-04 00:31:12 +0100

    enh: license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-wrapper-facade "0.1.0-SNAPSHOT"
   :description "a spike"
-  :license {:name "BSD3"
-            :url "https://opensource.org/licenses/BSD-3-Clause"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
